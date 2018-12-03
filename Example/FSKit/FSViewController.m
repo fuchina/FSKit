20a79ed4df5725745da78313daaf868aa1ca2eb6
扶冬冬 <fudongdong@meituan.com>
2018-12-03 20:08:45 +0800

    提醒逻辑梳理

--- Example/FSKit/FSViewController.m
+++ Example/FSKit/FSViewController.m
@@ -10,6 +10,19 @@
 #import "FSKit-umbrella.h"
 #import "FSModel.h"
 #import "FSSecondController.h"
+#import <FSDate.h>
+
+
+typedef NS_ENUM(NSInteger, FSWeekdayIndex) {
+    FSWeekdayIndexMonday = 1,
+    FSWeekdayIndexTuesday = 2,
+    FSWeekdayIndexWednesday = 3,
+    FSWeekdayIndexThursday = 4,
+    FSWeekdayIndexFriday = 5,
+    FSWeekdayIndexSaturday = 6,
+    FSWeekdayIndexSunday = 7,
+};
+
 
 @interface FSViewController ()
 
@@ -35,8 +48,47 @@
 //    FSSecondController *vc = [[FSSecondController alloc] init];
 //    [vc setText:@"我不会出现"];
 //    [self.navigationController pushViewController:vc animated:YES];
+    
+    NSDate *date = [FSDate dateByString:@"2018-12-02 23:00:00" formatter:nil];
+    NSDateComponents *c = [FSDate componentForDate:date];
+    NSLog(@"%@,%@",@(c.weekOfYear),@([self weekIndexForWeekday:c.weekday]));
 }
 
+
+/*
+ Type:5
+ 
+ Did:一个时间戳
+ Expitime:一个数字，1，2，3，4，5，6，7分表表示周一到周日；
+ 
+ 判断到期：
+ 1.获取当前时间now,用“年+第几周”得到一个数字‘201820’，前四位为年数，后两位为本年第几周；did，也同样得到‘201820’。
+    如果now跟did相等，不需要提示；否则，now比did大至少一周，判断now是星期几，如果now小于expitime,不提示；否则now比已经到达了该提示的时间。
+    1）左边显示“每周A提醒”，A为expitime对应的值汉化；
+    2）右边显示“已过期多少天”，天数由(now - did) * 7 + now.weekday - expitime + 1.
+ 
+ 2.未过期的显示：“将在下周三提醒”，“将在本周五提醒”，根据现在的是周几与expitime比较确定是下周还是本周。
+ 
+ 1.获取当前的时间，跟did时间比较，如果expitime小于等于did，返回；否则，获取当前时间的星期，如果该星期大于等于expitime，就应该提示。
+
+ */
+- (void)alertWeekly{
+    
+}
+
+/*
+    c.weekday中，周日是1，周一是2，周六是7；
+ */
+- (FSWeekdayIndex)weekIndexForWeekday:(NSInteger)weekday{
+    NSAssert(weekday > 0 && weekday < 8, @"weekIndexForWeekday:出现不正常的参数");
+    if (weekday > 0 && weekday < 8) { // 1~7
+        if (weekday == 1) {
+            return FSWeekdayIndexSunday;
+        }
+        return weekday - 1;
+    }
+    return FSWeekdayIndexSunday;
+}
 
 - (void)didReceiveMemoryWarning
 {
