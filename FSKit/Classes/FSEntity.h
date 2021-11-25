5d6255139435961164d015f09b890507ef64afa1
fudongdong <fudongdong@ihuman.com>
2021-11-25 14:41:20 +0800

    优化基类model

--- FSKit/Classes/FSEntity.h
+++ FSKit/Classes/FSEntity.h
@@ -1,6 +1,6 @@
 //
 //  FSEntity.h
-//  FSCompany
+//  fusuhehua.com
 //
 //  Created by 扶冬冬 on 2021/3/15.
 //
@@ -11,13 +11,11 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface FSEntity : NSObject
 
-@property (nonatomic, readonly) NSDictionary    *meta; // 原数据
-
-- (void)beforeInitWithDictionary;
+@property (nonatomic, readonly) NSDictionary    *meta;  // 原数据
 
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary;
 
--(void)setAttributes:(NSDictionary *)dataDic;
+- (void)setAttributes:(NSDictionary *)dictionary;
 
 - (void)afterSetAttributes;
 
--- FSKit/Classes/FSEntity.m
+++ FSKit/Classes/FSEntity.m
@@ -1,6 +1,6 @@
 //
-//  HEBaseModel.m
-//  OxfordLibrary
+//  FSEntity.m
+//  fusuhehua.com
 //
 //  Created by 扶冬冬 on 2021/3/15.
 //
@@ -10,31 +10,18 @@
 
 @implementation FSEntity
 
-- (instancetype)initWithDictionary:(NSDictionary *)dictionary{
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary {
     self = [super init];
     if (self) {
-        [self beforeInitWithDictionary];
-        if ([dictionary isKindOfClass:NSDictionary.class]) {
-            [self setAttributes:dictionary];
-        }
+        [self setAttributes:dictionary];
     }
     return self;
 }
 
-- (void)beforeInitWithDictionary {}
-
--(NSDictionary*)attributeMapDictionary{
-    return nil;
-}
-
-- (SEL)getSetterSelWithAttibuteName:(NSString*)attributeName{
+- (SEL)getSetterSelWithAttibuteName:(NSString *)attributeName {
     NSString *capital = [[attributeName substringToIndex:1] uppercaseString];
     NSString *setterSelStr = [NSString stringWithFormat:@"set%@%@:",capital,[attributeName substringFromIndex:1]];
     return NSSelectorFromString(setterSelStr);
-}
-
-- (NSString *)customDescription{
-    return nil;
 }
 
 + (NSArray *)modelsFromDictionaries:(NSArray<NSDictionary *> *)dictionaries modelClass:(Class)CLS {
@@ -62,7 +49,7 @@
             [results addObject:model];
         }
     }
-    return results;
+    return results.copy;
 }
 
 + (id)modelWithDictionary:(NSDictionary *)m modelClass:(Class)CLS {
@@ -77,165 +64,34 @@
     return obj;
 }
 
-- (NSString *)description{
-    NSMutableString *attrsDesc = [NSMutableString stringWithCapacity:100];
-    NSDictionary *attrMapDic = [self attributeMapDictionary];
-    NSEnumerator *keyEnum = [attrMapDic keyEnumerator];
-    id attributeName;
-    
-    while ((attributeName = [keyEnum nextObject])) {
-        SEL getSel = NSSelectorFromString(attributeName);
-        if ([self respondsToSelector:getSel]) {
-            NSMethodSignature *signature = [self methodSignatureForSelector:getSel];
-            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
-            [invocation setTarget:self];
-            [invocation setSelector:getSel];
-            NSObject *valueObj = nil;
-            [invocation invoke];
-            [invocation getReturnValue:&valueObj];
-            //            ITTDINFO(@"attributeName %@ value %@", attributeName, valueObj);
-            if (valueObj) {
-                [attrsDesc appendFormat:@" [%@=%@] ",attributeName, valueObj];
-                //[valueObj release];
-            }else {
-                [attrsDesc appendFormat:@" [%@=nil] ",attributeName];
-            }
-            
-        }
+- (void)setAttributes:(NSDictionary *)dataDic {
+    if (![dataDic isKindOfClass:NSDictionary.class]) {
+        return;
     }
-    
-    NSString *customDesc = [self customDescription];
-    NSString *desc;
-    
-    if (customDesc && [customDesc length] > 0 ) {
-        desc = [NSString stringWithFormat:@"%@:{%@,%@}",self.class,attrsDesc,customDesc];
-    }else {
-        desc = [NSString stringWithFormat:@"%@:{%@}",self.class,attrsDesc];
-    }
-    return desc;
-}
-
-- (void)setAttributes:(NSDictionary *)dataDic{
     _meta = dataDic;
     
-    NSDictionary *attrMapDic = [self attributeMapDictionary];
-    if (attrMapDic == nil) {
-        NSMutableDictionary *dic = [NSMutableDictionary dictionaryWithCapacity:[dataDic count]];
-        for (NSString *key in dataDic) {
-            [dic setValue:key forKey:key];
-        }
-        attrMapDic = dic;
-    }
-    
-    NSMutableArray *handleArray = [[NSMutableArray alloc] initWithArray:[self propertiesList]];
-    NSEnumerator *keyEnum = [attrMapDic keyEnumerator];
-    id attributeName;
-    while ((attributeName = [keyEnum nextObject])) {
-        SEL sel = [self getSetterSelWithAttibuteName:attributeName];
+    NSArray *allKeys = [dataDic allKeys];
+    for (NSString *key in allKeys) {
+        SEL sel = [self getSetterSelWithAttibuteName:key];
         if ([self respondsToSelector:sel]) {
-            NSString *dataDicKey = [attrMapDic objectForKey:attributeName];
-            NSObject *value = [dataDic objectForKey:dataDicKey];
+            NSObject *value = [dataDic objectForKey:key];
             if ([value isKindOfClass:NSArray.class] || [value isKindOfClass:NSDictionary.class] || [value isKindOfClass:NSString.class]) {
-            }else{
+                
+            } else {
                 if (value == nil || [value isEqual:NSNull.null]) {
-//                     value = @"";
                     value = nil;
-                }else{
-                    value = value.description;// nil变量调用description仍然为nil
+                } else {
+                    value = value.description;
                 }
             }
-            id attributeValue = value;
-            [self performSelector:sel onThread:NSThread.currentThread withObject:attributeValue waitUntilDone:YES];
-            
-            if ([handleArray containsObject:attributeName]) {
-                [handleArray removeObject:attributeName];
-            }
+
+            [self performSelector:sel onThread:NSThread.currentThread withObject:value waitUntilDone:YES];
         }
     }
-    
-    // 没必要赋值nil
-//    if (handleArray) { // 未赋值的参数赋值nil
-//        for (int x = 0; x < handleArray.count; x ++) {
-//            NSString *nameString = handleArray[x];
-//            if (nameString) {
-//                SEL sel = [self getSetterSelWithAttibuteName:nameString];
-//                if ([self respondsToSelector:sel]) {
-//                    [self performSelector:sel onThread:NSThread.currentThread
-//                               withObject:nil   //类型不匹配，不会崩溃
-//                            waitUntilDone:YES];
-//                }
-//            }
-//        }
-//    }
     
     [self afterSetAttributes];
 }
 
 - (void)afterSetAttributes {}
-
-- (id)initWithCoder:(NSCoder *)decoder{
-    if( self = [super init] ){
-        NSDictionary *attrMapDic = [self attributeMapDictionary];
-        if (attrMapDic == nil) {
-            return self;
-        }
-        NSEnumerator *keyEnum = [attrMapDic keyEnumerator];
-        id attributeName;
-        while ((attributeName = [keyEnum nextObject])) {
-            SEL sel = [self getSetterSelWithAttibuteName:attributeName];
-            if ([self respondsToSelector:sel]) {
-                id obj = [decoder decodeObjectForKey:attributeName];
-                [self performSelector:sel onThread:NSThread.currentThread withObject:obj waitUntilDone:YES];
-            }
-        }
-    }
-    return self;
-}
-
-- (void)encodeWithCoder:(NSCoder *)encoder{
-    NSDictionary *attrMapDic = [self attributeMapDictionary];
-    if (attrMapDic == nil) {
-        return;
-    }
-    
-    NSEnumerator *keyEnum = [attrMapDic keyEnumerator];
-    id attributeName;
-    while ((attributeName = [keyEnum nextObject])) {
-        SEL getSel = NSSelectorFromString(attributeName);
-        if ([self respondsToSelector:getSel]) {
-            NSMethodSignature *signature = nil;
-            signature = [self methodSignatureForSelector:getSel];
-            NSInvocation *invocation = [NSInvocation invocationWithMethodSignature:signature];
-            [invocation setTarget:self];
-            [invocation setSelector:getSel];
-            NSObject *valueObj = nil;
-            [invocation invoke];
-            [invocation getReturnValue:&valueObj];
-            
-            if (valueObj) {
-                [encoder encodeObject:valueObj forKey:attributeName];
-            }
-        }
-    }
-}
-
-- (NSData*)getArchivedData{
-    return [NSKeyedArchiver archivedDataWithRootObject:self];
-}
-
-- (NSArray *)propertiesList{
-    NSMutableArray *array = [[NSMutableArray alloc] init];
-    unsigned int propertyCount = 0;
-    objc_property_t *properties = class_copyPropertyList(self.class, &propertyCount);
-    
-    for (unsigned int i = 0; i < propertyCount; ++i) {
-        objc_property_t property = properties[i];
-        const char *name = property_getName(property);//获取属性名字
-        
-        NSString *nameToString = [[NSString alloc] initWithFormat:@"%s",name];
-        [array addObject:nameToString];
-    }
-    return array;
-}
 
 @end
