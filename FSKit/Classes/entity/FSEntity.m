53d0a0ad8ba639a689aad0907b232e8e95ebe25f
fuchina <1245102331@qq.com>
2023-07-13 22:10:21 +0800

    model

--- FSKit/Classes/entity/FSEntity.m
+++ FSKit/Classes/entity/FSEntity.m
@@ -6,19 +6,36 @@
 //
 
 #import "FSEntity.h"
-#import <objc/runtime.h>
+
+@interface FSEntity ()
+
+@property (nonatomic, copy) void (^config)(id self_object);
+
+@end
 
 @implementation FSEntity
 
+- (void)beforeSetProperties {}
+
 - (instancetype)initWithDictionary:(NSDictionary *)dictionary {
+    return [self initWithDictionary:dictionary config:nil];
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)dictionary config:(void(^)(id model))config {
     self = [super init];
     if (self) {
-        [self setAttributes:dictionary];
+        [self beforeSetProperties];
+        _config = config;
+        [self setProperties:dictionary];
     }
     return self;
 }
 
 + (NSArray *)modelsFromDictionaries:(NSArray<NSDictionary *> *)dictionaries modelClass:(Class)CLS {
+    return [self modelsFromDictionaries:dictionaries modelClass:CLS config:nil];
+}
+
++ (NSArray *)modelsFromDictionaries:(NSArray<NSDictionary *> *)dictionaries modelClass:(Class)CLS config:(nullable void(^)(id model))config {
     BOOL isArray = [dictionaries isKindOfClass:NSArray.class];
     if (isArray) {
         // 如果元素不是NSDictionary，直接返回
@@ -39,7 +56,7 @@
         results = [[NSMutableArray alloc] initWithCapacity:dictionaries.count];
         for (int x = 0; x < dictionaries.count; x ++) {
             NSDictionary *m = dictionaries[x];
-            id model = [self modelWithDictionary:m modelClass:CLS];
+            id model = [self modelWithDictionary:m modelClass:CLS config:config];
             [results addObject:model];
         }
     }
@@ -47,34 +64,43 @@
 }
 
 + (id)modelWithDictionary:(NSDictionary *)m modelClass:(Class)CLS {
+    return [self modelWithDictionary:m modelClass:CLS config:nil];
+}
+
++ (id)modelWithDictionary:(NSDictionary *)m modelClass:(Class)CLS config:(nullable void(^)(id model))config {
     id obj = nil;
     if ([m isKindOfClass:NSDictionary.class]) {
         obj = [[CLS alloc] init];
         if ([obj isKindOfClass:FSEntity.class]) {
             FSEntity *model = (FSEntity *)obj;
-            [model setAttributes:m];
+            model.config = config;
+            [model setProperties:m];
         }
     }
     return obj;
 }
 
-- (void)setAttributes:(NSDictionary *)dataDic {
+- (void)setProperties:(NSDictionary *)dataDic {
     if (![dataDic isKindOfClass:NSDictionary.class]) {
         return;
     }
     _meta = dataDic;
-        
+    
+    if (_config) {
+        _config(self);
+    }
+    
     NSArray *allKeys = [dataDic allKeys];
     for (NSString *key in allKeys) {
         NSObject *value = [dataDic objectForKey:key];
         [self setValue:value forKey:key];
     }
     
-    [self afterSetAttributes];
+    [self afterSetProperties];
 }
 
-- (void)setValue:(id)value forKey:(NSString *)key {    
-    if ([value isKindOfClass:NSString.class] || [value isKindOfClass:NSNumber.class] || [value isKindOfClass:NSArray.class] || [value isKindOfClass:NSDictionary.class]) {
+- (void)setValue:(id)value forKey:(NSString *)key {
+    if ([value isKindOfClass:NSArray.class] || [value isKindOfClass:NSDictionary.class] || [value isKindOfClass:NSString.class]) {
         
     } else {
         if (value == nil || [value isEqual:NSNull.null]) {
@@ -87,11 +113,11 @@
     [super setValue:value forKey:key];
 }
 
-- (void)afterSetAttributes {}
+- (void)afterSetProperties {}
 
 - (void)setValue:(id)value forUndefinedKey:(NSString *)key {
 #if DEBUG
-    NSLog(@"FSEntity set UndefinedKey (%@ - %@)", key, value);
+    NSLog(@"HEBaseModel set UndefinedKey (%@ - %@)", key, value);
 #endif
 }
 
