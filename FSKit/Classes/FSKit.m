a02875e61e9cc692271b652a06da92e3fcfda79f
fuchina <1245102331@qq.com>
2024-04-27 15:33:42 +0800

    Update FSKit.m

--- FSKit/Classes/FSKit.m
+++ FSKit/Classes/FSKit.m
@@ -1048,7 +1048,10 @@ NSString *_fs_md5(NSString *str) {
     }
     const char *cStr = [str UTF8String];
     unsigned char result[CC_MD5_DIGEST_LENGTH];
-    CC_MD5(cStr, (CC_LONG)strlen(cStr),result);
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    CC_MD5(cStr, (CC_LONG)strlen(cStr),result);  // 什么时候会失效呢？2024.04.27
+#pragma clang diagnostic pop
     return [NSString  stringWithFormat:
             @"%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X%02X",
             result[0], result[1], result[2], result[3], result[4],
