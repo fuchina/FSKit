a459dab847e57e1edb6c7c24a855900aef692f98
fuchina <1245102331@qq.com>
2026-01-25 13:51:50 +0800

    Create FSEntity.swift

---
+++ FSKit/Classes/entity/FSEntity.swift
@@ -0,0 +1,13 @@
+//
+//  FSEntity.swift
+//  FSKit
+//
+//  Created by Dongdong Fu on 2026/1/25.
+//
+
+import Foundation
+
+class FSEntity: Codable {
+    public var     aid     :   Int
+    public var     meta    :   Dictionary<String, String>
+}
