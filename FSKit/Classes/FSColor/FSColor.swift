1923147f78b550f0df832888737c9d116496a683
fudongdong <1245102331@qq.com>
2026-09-14 19:53:13 +0800

    Create FSColor.swift

---
+++ FSKit/Classes/FSColor/FSColor.swift
@@ -0,0 +1,58 @@
+//
+//  FSColor.swift
+//  FSAppPublic
+//
+//  Created by FudonFuchina on 2019/10/20.
+//  Translated to Swift
+//
+
+import UIKit
+
+/// 用于处理颜色
+public struct FSColor {
+    
+    // MARK: - 品牌色
+    
+    /// 微信绿
+    public static let wechatGreen = UIColor(red: 0x20 / 255.0, green: 0xbf / 255.0, blue: 0x66 / 255.0, alpha: 1)
+    
+    /// 美团黄
+    public static let meituanYellow = UIColor(red: 1, green: 195 / 255.0, blue: 0, alpha: 1)
+    
+    /// Apple News 红
+    public static let appleNewsRed = UIColor(red: 250 / 255.0, green: 80 / 255.0, blue: 100 / 255.0, alpha: 1)
+    
+    /// 支付宝蓝
+    public static let alipayBlue = UIColor(red: 18 / 255.0, green: 152 / 255.0, blue: 233 / 255.0, alpha: 1)
+    
+    /// 微信朋友圈蓝
+    public static let wechatFriendsBlue = UIColor(red: 0x58 / 255.0, green: 0x6c / 255.0, blue: 0x93 / 255.0, alpha: 1)
+    
+    /// 微信朋友圈蓝（带透明度）
+    public static let wechatFriendsBlueAlpha = UIColor(red: 0x58 / 255.0, green: 0x6c / 255.0, blue: 0x93 / 255.0, alpha: 0.1)
+    
+    /// iOS 系统蓝
+    public static let iosSystemBlue = UIColor(red: 0, green: 0x7A / 255.0, blue: 0xFF / 255.0, alpha: 1)
+    
+    /// App Store 按钮背景色（淡蓝色）
+    public static let appStoreButtonBackground = UIColor(red: 0xf0 / 255.0, green: 0xf0 / 255.0, blue: 0xf7 / 255.0, alpha: 1)
+    
+    // MARK: - 基础色
+    
+    /// 红色
+    public static let red = UIColor(red: 250 / 255.0, green: 49 / 255.0, blue: 105 / 255.0, alpha: 1)
+    
+    /// 黄色
+    public static let yellow = UIColor(red: 253 / 255.0, green: 111 / 255.0, blue: 55 / 255.0, alpha: 1)
+    
+    // MARK: - 文字颜色
+    
+    /// 深色文字
+    public static let textDark = UIColor(red: 16 / 255.0, green: 16 / 255.0, blue: 16 / 255.0, alpha: 1)
+    
+    /// 普通文字
+    public static let textNormal = UIColor(red: 88 / 255.0, green: 88 / 255.0, blue: 88 / 255.0, alpha: 1)
+    
+    /// 浅色文字
+    public static let textLight = UIColor(red: 169 / 255.0, green: 169 / 255.0, blue: 169 / 255.0, alpha: 1)
+}
